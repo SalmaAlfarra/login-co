3bcca7f4e5c7de6559e59894adc3903fdd83d23c
mhmoudsh <mmh1999.2014@gmail.com>
2022-12-03 11:54:01 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+
+<!DOCTYPE html>
 <!--
 Template Name: Metronic - Bootstrap 4 HTML, React, Angular 11 & VueJS Admin Dashboard Theme
 Author: KeenThemes
@@ -11,35 +12,41 @@ Purchase: https://1.envato.market/EA4JP
 Renew Support: https://1.envato.market/EA4JP
 License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
 -->
-<html lang="en">
+{{-- direction="rtl" dir="rtl" style="direction: rtl" --}}
+<html lang="ar">
 	<!--begin::Head-->
-	<head><base href="../../../../">
+	<head>
 		<meta charset="utf-8" />
-		<title>Login Page 5 | Keenthemes</title>
-		<meta name="description" content="Login page example" />
+		<title>شركة لوج إن للتنمية و الاستثمار </title>
+		<meta name="description" content="Page with empty content" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-		<link rel="canonical" href="https://keenthemes.com/metronic" />
 		<!--begin::Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 		<!--end::Fonts-->
-		<!--begin::Page Custom Styles(used by this page)-->
-		<link href="assets/css/pages/login/classic/login-5.css" rel="stylesheet" type="text/css" />
-		<!--end::Page Custom Styles-->
+		<!--begin::Page Vendors Styles(used by this page)-->
+		<link href="{{asset('admin/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
+		<!--end::Page Vendors Styles-->
 		<!--begin::Global Theme Styles(used by all pages)-->
-		<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-		<link href="assets/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
-		<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/plugins/global/plugins.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/plugins/custom/prismjs/prismjs.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/css/style.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
 		<!--end::Global Theme Styles-->
 		<!--begin::Layout Themes(used by all pages)-->
-		<link href="assets/css/themes/layout/header/base/light.css" rel="stylesheet" type="text/css" />
-		<link href="assets/css/themes/layout/header/menu/light.css" rel="stylesheet" type="text/css" />
-		<link href="assets/css/themes/layout/brand/dark.css" rel="stylesheet" type="text/css" />
-		<link href="assets/css/themes/layout/aside/dark.css" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/css/themes/layout/header/base/dark.rtl.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/css/themes/layout/header/menu/dark.rtl.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/css/themes/layout/brand/dark.rtl.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{asset('admin/assets/css/themes/layout/aside/dark.rtl.css')}}" rel="stylesheet" type="text/css" />
+        
+        @yield('css')
+			<style>
+				.media-image {
+					height: 30px!important; background-size: contain; background-repeat: no-repeat; background-position: center center; width: 30px!important;
+				}
+			</style>
 		<!--end::Layout Themes-->
-		<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+		<link rel="shortcut icon" href="{{asset('admin/assets/media/logos/favicon.ico')}}" />
 	</head>
-	<!--end::Head-->
-	<!--begin::Body-->
+
 	<body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
 		<!--begin::Main-->
 		<div class="d-flex flex-column flex-root">
@@ -60,7 +67,8 @@ License: You must have a valid license purchased only from themeforest(the above
 								<h3 class="opacity-40 font-weight-normal">Sign In To Admin</h3>
 								<p class="opacity-40">Enter your details to login to your account:</p>
 							</div>
-							<form class="form" id="kt_login_signin_form">
+							<form method="POST" action="{{route('login') }}" class="form" id="kt_login_signin_form">
+								@csrf
 								<div class="form-group">
 									<input class="form-control h-auto text-white bg-white-o-5 rounded-pill border-0 py-4 px-8" type="text" placeholder="Email" name="username" autocomplete="off" />
 								</div>
@@ -73,7 +81,11 @@ License: You must have a valid license purchased only from themeforest(the above
 										<input type="checkbox" name="remember" />
 										<span></span>Remember me</label>
 									</div>
-									<a href="javascript:;" id="kt_login_forgot" class="text-white font-weight-bold">Forget Password ?</a>
+									 @if (Route::has('password.request'))
+										<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+											{{ __('Forgot your password?') }}
+										</a>
+               						 @endif
 								</div>
 								<div class="form-group text-center mt-10">
 									<button id="kt_login_signin_submit" class="btn btn-pill btn-primary opacity-90 px-15 py-3">Sign In</button>
@@ -142,19 +154,23 @@ License: You must have a valid license purchased only from themeforest(the above
 			</div>
 			<!--end::Login-->
 		</div>
+
 		<!--end::Main-->
-		<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+
+	</body>
+
+        <script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
 		<!--begin::Global Config(global config for global JS scripts)-->
 		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
 		<!--end::Global Config-->
 		<!--begin::Global Theme Bundle(used by all pages)-->
-		<script src="assets/plugins/global/plugins.bundle.js"></script>
-		<script src="assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
-		<script src="assets/js/scripts.bundle.js"></script>
+		<script src="{{asset('admin/assets/plugins/global/plugins.bundle.js')}}"></script>
+		<script src="{{asset('admin/assets/plugins/custom/prismjs/prismjs.bundle.js')}}"></script>
+		<script src="{{asset('admin/assets/js/scripts.bundle.js')}}"></script>
 		<!--end::Global Theme Bundle-->
 		<!--begin::Page Scripts(used by this page)-->
-		<script src="assets/js/pages/custom/login/login-general.js"></script>
+		<script src="{{asset('admin/assets/js/pages/custom/login/login-general.js')}}"></script>
 		<!--end::Page Scripts-->
-	</body>
+
 	<!--end::Body-->
 </html>
--- routes/web.php
+++ routes/web.php
@@ -113,3 +113,19 @@ Route::post('/patronexcel', [PatronController::class, 'patronuploadexcel'])->nam
 Route::get('/customer-profile', function () {
     return view('customer.customer-profile');
 });
+Route::get('/', function () {
+return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
